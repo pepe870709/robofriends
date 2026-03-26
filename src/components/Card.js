80ea7818b29d1d3f862892d57fbd2bce4a00981f
pepe870709dev <josehernandezdev09@gmail.com>
2026-03-25 22:45:12 -0500

    My first contribution ppp

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Card = ({ name, email, id }) => {
+
+  console.log(name)
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>
+        <hr></hr>
         <p>{email}</p>
       </div>
     </div>
