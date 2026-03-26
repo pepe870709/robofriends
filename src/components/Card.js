a70e75bf7cd233d3205edd1b59941f10dc59b9f0
pepe870709dev <josehernandezdev09@gmail.com>
2026-03-25 23:11:43 -0500

    My thirds contribution ppp

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ const Card = ({ name, email, id }) => {
 
   console.log(name)
   console.log(email)
+  console.log(id)
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
